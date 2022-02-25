c052d31c6aac65172326def20f190003447a8de8
raden-dimas012 <dimascode012@gmail.com>
2022-02-26 01:04:13 +0700

    Added function and variabel to Person Class

--- BinarAcademy.playground/Pages/Challenge OOP.xcplaygroundpage/Contents.swift
+++ BinarAcademy.playground/Pages/Challenge OOP.xcplaygroundpage/Contents.swift
@@ -4,11 +4,102 @@ import Foundation
 import UIKit
 
 //: [Next](@next)
+
+enum Role {
+    case iOSDeveloper,androidDeveloper
+}
+
+
 class Person {
+    var skinColor: UIColor
+    var eyeColor: UIColor
+    var nation: String?
+    var role: Role
+    var salary: Double?
+    var jobLevel: String?
+    var yearsOfExperience: Int
     
-    func something() {
+   
+    
+    init(skinColor: UIColor,eyeColor: UIColor,role: Role,yearsOfExperience: Int) {
+        self.skinColor = skinColor
+        self.eyeColor = eyeColor
+        self.role = role
+        self.yearsOfExperience = yearsOfExperience
+    }
+    
+    private func countSalary(n: Int) -> Double? {
+        if role == .iOSDeveloper {
+            if n > 5 {
+                var resSalary = self.salary ?? 0.0
+                resSalary += 20000000.600
+                salary = resSalary
+                countSalary(n: n - 5)
+                return salary
+            } else {
+                var resSalary = self.salary ?? 0.0
+                resSalary += Double(n) * 2500000
+                salary = resSalary
+                return salary
+            }
+        } else {
+            if n > 5 {
+                var resSalary = self.salary ?? 0.0
+                resSalary += 18000000.600
+                salary = resSalary
+                countSalary(n: n - 5)
+                return salary
+            } else {
+                var resSalary = self.salary ?? 0.0
+                resSalary += Double(n) * 2200000
+                salary = resSalary
+                return salary
+            }
+        }
+    }
+
+    
+    func identifyPerson() -> (resNation:String?,resSalary:Double?,resJobLevel: String?) {
+        
+        let resultNation = identifyNation()
+        let resultJobLevel = identifyJobLevel()
+        let resultSalary = countSalary(n: yearsOfExperience)
+        
+        
+        return ( resultNation,resultSalary,resultJobLevel )
+    }
+    
+    private func identifyJobLevel() -> String? {
+        if yearsOfExperience > 5 {
+            jobLevel = "Lead"
+        } else if yearsOfExperience < 5 && yearsOfExperience > 3 {
+            jobLevel = "Senior"
+        } else {
+            jobLevel = "Junior"
+        }
+        return jobLevel
+       
+    }
+    
+    
+    private func identifyNation() -> String? {
+        if skinColor == .white && eyeColor == .black {
+            nation = "Indonesia"
+        } else {
+            nation = "Not Indonesia"
+        }
+        
+        return nation
     }
 }
 
-let person = Person()
-something()
+let indonesian = Person(skinColor: .white,eyeColor: .black,role: Role.iOSDeveloper,yearsOfExperience: 12)
+
+print(indonesian.identifyPerson())
+
+
+
+
+
+
+//: [Next](@next)
