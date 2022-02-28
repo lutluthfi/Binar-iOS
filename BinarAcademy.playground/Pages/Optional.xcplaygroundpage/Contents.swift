8fabb6fd7a6eac103d6d05b042e00ed228b8c5ca
Prana Apsara Wijaya <42761991+pranahonk@users.noreply.github.com>
2022-02-28 19:51:42 +0700

    Add new documentation about parameters (#1)
    add variadic parameters and inout parameters (#2)

--- BinarAcademy.playground/Pages/Optional.xcplaygroundpage/Contents.swift
+++ BinarAcademy.playground/Pages/Optional.xcplaygroundpage/Contents.swift
@@ -8,3 +8,30 @@ var greeting = "Hello, playground"
 
 var number: Double = 11.0
 var _number = Int(number)
+
+
+//Using variadic parameters
+func sayHello(greeting: String, names: String...) {
+    for name in names {
+        print("\(greeting) \(name)")
+    }
+}
+
+
+sayHello(greeting:"Hello", names: "Jon", "Kara")
+
+//Hello Jon
+//Hello Kara
+
+
+//inout parameters
+func reverse(first: inout String, second: inout String) {
+    let tmp = first
+    first = second
+    second = tmp
+}
+
+var one = "One"
+var two = "Two"
+reverse(first: &one, second: &two)
+print("one: \(one) two: \(two)")
