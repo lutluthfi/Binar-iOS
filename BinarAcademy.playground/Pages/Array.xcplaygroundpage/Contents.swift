ea5d78959174cba7bc6caac633c5315edd056fef
raden-dimas012 <dimascode012@gmail.com>
2022-03-10 17:27:41 +0700

    added zip function to array page

--- BinarAcademy.playground/Pages/Array.xcplaygroundpage/Contents.swift
+++ BinarAcademy.playground/Pages/Array.xcplaygroundpage/Contents.swift
@@ -29,3 +29,15 @@ tambahBuah(10)
 // Case Insensitive => aa == AA == Aa == aA
 //
 //
+
+
+// zip -> merge two of arrays
+
+let array1: [String] = ["test1","test2","test3","test4"]
+//let array2: ClosedRange<Int> = 1...5
+let array3: [Int] = [1,3,4,5]
+let array4: Set = ["test1","test2"]
+let array5: Set = [1.0,5.4,6.6]
+
+let zipped = Array(zip(array4, array5))
+print(zipped)
--- BinarAcademy.playground/Pages/Inheritance.xcplaygroundpage/Contents.swift
+++ BinarAcademy.playground/Pages/Inheritance.xcplaygroundpage/Contents.swift
@@ -24,15 +24,16 @@ class Creature { // Parent Class
 
 class Animal: Creature { // Child Class
     //Can't override var because it set as final
-    override var fullName: String { "Animal - \(name)" }
+//    override var fullName: String { "Animal - \(name)" }
     
     override func fromCreature() {
     }
 }
 
 //Can't inheritance Struct from Class
-struct Human: Creature { // Inheritance from non-protocol type
-}
+//struct Human: Creature {
+    // Inheritance from non-protocol type
+//}
 
 let cat = Animal()
 cat.habitat
