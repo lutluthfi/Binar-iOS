8fefe7ceb161fa9c62c6c46f3c1f505e012152bb
prana.wijaya <prana.wijaya@mncgroup.com>
2022-03-15 21:12:25 +0700

    fixing PWViewController

--- Binar/Binar/PW/PWViewController.swift
+++ Binar/Binar/PW/PWViewController.swift
@@ -5,15 +5,15 @@
 //  Created by user on 15/03/22.
 //
 
-import Foundation
 import UIKit
-
 
 final class PWViewController: UIViewController {
     var name: String?
+    @IBOutlet weak var myName: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = name
+        myName.text = name
     }
 }
