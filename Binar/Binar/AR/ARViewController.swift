a7b4d298e05cdb35b3dadb5b79d86dd089106933
Arif Luthfiansyah <lutluthfiansyah@gmail.com>
2022-03-16 20:08:15 +0700

    Implement AlertController

--- Binar/Binar/AR/ARViewController.swift
+++ Binar/Binar/AR/ARViewController.swift
@@ -8,10 +8,32 @@
 import UIKit
 
 final class ARViewController: UIViewController {
+    @IBOutlet weak var textLabel: UILabel!
+    
     var name: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = name
+    }
+    
+    @IBAction func onAlertButtonTap(_ sender: UIButton) {
+        let alertController = UIAlertController(title: "Alert", message: "This is message's alert", preferredStyle: .alert)
+        
+        alertController.addTextField { textField in
+            textField.placeholder = "Search"
+        }
+        
+        let cancel = UIAlertAction(title: "Cancel", style: .destructive)
+        alertController.addAction(cancel)
+        
+        let confirm = UIAlertAction(title: "Confirm", style: .default) { action in
+            let textField: UITextField? = alertController.textFields?[0]
+            let text: String? = textField?.text
+            self.textLabel.text = text
+        }
+        alertController.addAction(confirm)
+        
+        present(alertController, animated: true)
     }
 }
