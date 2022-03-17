c09eb679f97d35f428cb356adac5d6280bd3f6ec
Bagas Ilham <rabbanilham@gmail.com>
2022-03-17 20:26:05 +0700

    add randomize function

--- Binar/Binar/BI/BIViewController.swift
+++ Binar/Binar/BI/BIViewController.swift
@@ -9,16 +9,29 @@ import UIKit
 
 final class BIViewController: UITableViewController {
     @IBOutlet weak var AnimalSearchBar: UISearchBar!
+    @IBOutlet weak var randomAnimal: UIBarButtonItem!
     var name: String?
-    var selectedAnimal: Animal?
-    var displayedAnimals: [String] = Animal.list
+
+
+    @IBAction func onRandomizeTouchUpInside(_ sender: UIButton) {
+        let randomAnimal: String = Animal.list.randomElement()!.lowercased()
+        let alertController = UIAlertController(title: "Wow!", message: "You are a \(randomAnimal)!", preferredStyle: .alert)
+        
+        let cancel = UIAlertAction(title: "Great!", style: .default)
+        alertController.addAction(cancel)
+                
+        present(alertController, animated: true)
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = name
+        title = name! + " Animal List"
     }
-    
+    var selectedAnimal: Animal?
+    var displayedAnimals: [String] = Animal.list
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
         let animalCount: Int = displayedAnimals.count
         return animalCount
     }
@@ -32,29 +45,23 @@ final class BIViewController: UITableViewController {
     }
 }
 
-//extension BIViewController: UISearchBarDelegate {
-//    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-//        
-//    }
-//    
-//    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
-//        tableView.endEditing(true)
-//    }
-//    
-//    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-//        let _searchText: String = searchText.trimmingCharacters(in: .whitespacesAndNewlines)
-//        let isSearchTextNotEmpty = !_searchText.isEmpty
-//        let animals: [String] = Animal.list
-//        if isSearchTextNotEmpty {
-//            let searchedAnimals: [String] = animals.filter {
-//                let _searchText: String = searchText.lowercased()
-//                let _name: String = list.lowercased()
-//                return _name.contains(_searchText)
-//            }
-//            displayedAnimals = searchedAnimals
-//        } else {
-//            displayedAnimals = animals
-//        }
-//        tableView.reloadData()
-//    }
-//}
+extension BIViewController: UISearchBarDelegate {
+
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        let animalSearchText: String = searchText.trimmingCharacters(in: .whitespacesAndNewlines)
+        let isSearchTextNotEmpty = !animalSearchText.isEmpty
+        let animals: [String] = Animal.list
+        if isSearchTextNotEmpty {
+            let searchedAnimals: [String] = animals.filter {
+                let animalSearch: String = animalSearchText.lowercased()
+                let animalName: String = $0.lowercased()
+                return animalName.contains(animalSearch)
+            }
+            displayedAnimals = searchedAnimals
+        } else {
+            displayedAnimals = animals
+        }
+        tableView.reloadData()
+    }
+    
+}
