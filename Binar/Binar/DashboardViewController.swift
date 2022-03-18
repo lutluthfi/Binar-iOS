587f8cfb2e09200021ab22e54c27d83c7d041450
Daffashiddiq <daffashiddiq@gmail.com>
2022-03-18 21:45:12 +0700

    add animal challenge files

--- Binar/Binar/DashboardViewController.swift
+++ Binar/Binar/DashboardViewController.swift
@@ -53,6 +53,8 @@ final class DashboardViewController: UITableViewController {
             goToDPViewController()
         case .TatangSulaeman:
             goToTSViewController()
+        case .Daffashiddiq:
+            goToDSViewController()
         default:
             break
         }
@@ -195,6 +197,19 @@ extension DashboardViewController {
         guard let selectedStudent = selectedStudent else { return }
         let storyboard = UIStoryboard(name: "TSMain", bundle: nil)
         guard let viewController = storyboard.instantiateViewController(withIdentifier: "TSViewController") as? TSViewController else {
+            return
+        }
+
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+}
+
+// MARK: goToDSViewController
+extension DashboardViewController {
+    func goToDSViewController() {
+        guard let selectedStudent = selectedStudent else { return }
+        let storyboard = UIStoryboard(name: "DSMain", bundle: nil)
+        guard let viewController = storyboard.instantiateViewController(withIdentifier: "DSViewController") as? DSViewController else {
             return
         }
 
