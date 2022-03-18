b6227780816bbf943347159a1317db860a22b31b
prana.wijaya <prana.wijaya@mncgroup.com>
2022-03-18 10:26:41 +0700

    init tableviewcontroller

---
+++ Binar/Binar/PW/CustomTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CustomTableViewCell.swift
+//  Binar
+//
+//  Created by user on 17/03/22.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
+}
--- Binar/Binar/PW/PWViewController.swift
+++ Binar/Binar/PW/PWViewController.swift
@@ -2,63 +2,65 @@
 //  PWViewController.swift
 //  Binar
 //
-//  Created by user on 15/03/22.
+//  Created by user on 15/03/22.k
 //
 
 import UIKit
 import CardSlider
+import Kingfisher
 
 
-struct Item: CardSliderItem {
-    var image: UIImage
-
-    var title: String
-    var rating: Int?
-    var subtitle: String?
-    var description: String?
-}
-
-class PWViewController: UIViewController, CardSliderDataSource {
-    var data = [Item]()
-
-    @IBOutlet weak var myName: UILabel!
-    @IBOutlet weak var welcomeView1: UITextView!
-    @IBOutlet weak var welcomeView2: UITextView!
-    @IBOutlet weak var showButton: UIButton!
-
+class PWViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var table: UITableView!
+    
+    struct AnimalName {
+        let tilte: String
+        let imageName: String
+    }
+    
+    
+    var data: [AnimalName] = [];
+    
+    let url = URL(string: "https://cdn.cocoacasts.com/cc00ceb0c6bff0d536f25454d50223875d5c79f1/above-the-clouds.jpg")!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        table.dataSource = self
+        
+       
+        
+       
+        
         let elm = Animal.list
         let mirror = Mirror(reflecting: elm)
         for child in mirror.children  {
-            data.append(Item( image: UIImage(systemName: "heart.fill")!, title: child.value as! String , rating: 4, subtitle: "test", description: "test"))
-
+            data.append(AnimalName(tilte: child.value as! String, imageName: "IKEA-Logo"))
         }
 
-        guard let dataSource = self as? CardSliderDataSource else{
-            return
-        }
-
-
-//        welcomeView1.font = UIFont(name: "circular-std-medium-500", size: 24)
-//        welcomeView2.font = UIFont(name: "circular-std-medium-500", size: 24.0)
-//        welcomeView2.font = welcomeView2.font?.withSize(24)
-
     }
-
-    @IBAction func didTapButton(_ sender: Any) {
-        let vc = CardSliderViewController.with(dataSource: self)
-        vc.title = "Welcome!"
-        vc.modalPresentationStyle = .fullScreen
-        present(vc, animated: true)
-    }
-
-    func item(for index: Int) -> CardSliderItem {
-        return data[index]
-    }
-
-    func numberOfItems() -> Int {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return data.count
     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let animal = data[indexPath.row]
+        let cell = table.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomTableViewCell
+        
+        cell.label.text = animal.tilte
+        cell.iconImageView.image = UIImage(named: animal.imageName)
+//        if let data = try? Data(contentsOf: url) {
+//            // Create Image and Update Image View
+//
+//        }
+    
+        
+        return cell
+    }
+
+
 }
+
+
