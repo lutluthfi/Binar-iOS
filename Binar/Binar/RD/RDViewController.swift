e5c754f145ed37457ef2d9a479df3de491baca1a
raden-dimas012 <dimascode012@gmail.com>
2022-03-21 15:42:18 +0700

    added tableview and searchbar

--- Binar/Binar/RD/RDViewController.swift
+++ Binar/Binar/RD/RDViewController.swift
@@ -9,10 +9,95 @@ import UIKit
 
 final class RDViewController: UIViewController {
     var name: String?
+    var displayedStudents: [String] = Animal.list.sorted()
+    
+    let tableview: UITableView = {
+          let tv = UITableView()
+          tv.backgroundColor = UIColor.white
+          tv.translatesAutoresizingMaskIntoConstraints = false
+          return tv
+      }()
+    
+    let dayLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Day 1"
+        label.textColor = UIColor.white
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let searchVC = UISearchController(searchResultsController: nil)
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = name
         self.view.backgroundColor = .white
+        setupTableView()
+        createSearchBar()
     }
+    
+    func setupTableView() {
+        tableview.delegate = self
+        tableview.dataSource = self
+        tableview.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+        
+        view.addSubview(tableview)
+        
+        NSLayoutConstraint.activate([
+            tableview.topAnchor.constraint(equalTo: self.view.topAnchor),
+            tableview.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            tableview.rightAnchor.constraint(equalTo: self.view.rightAnchor),
+            tableview.leftAnchor.constraint(equalTo: self.view.leftAnchor)
+        ])
+    }
+    
+    func createSearchBar() {
+        navigationItem.searchController = searchVC
+        searchVC.searchBar.delegate = self
+    }
+    
+    
+    
+}
+
+extension RDViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let text = searchBar.text,!text.isEmpty else {
+            return
+        }
+        print(text)
+    }
+}
+
+extension RDViewController: UITableViewDelegate,UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return displayedStudents.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableview.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        cell.backgroundColor = UIColor.white
+        cell.textLabel?.text = displayedStudents[indexPath.row]
+        return cell
+    }
+}
+
+class ThirtyDayCell: UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
