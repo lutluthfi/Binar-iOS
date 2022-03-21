337da04695f81c2b8803a9a2141a4e23cabc072b
Nuzulul Athaya <nzllathy@icloud.com>
2022-03-21 18:38:07 +0700

    add animals tableview

--- Binar/Binar/AT/ATViewController.swift
+++ Binar/Binar/AT/ATViewController.swift
@@ -7,13 +7,57 @@
 
 import UIKit
 
-class ATViewController: UIViewController {
+final class ATViewController: UITableViewController {
 
+    @IBOutlet weak var searchAnimalBar: UISearchBar!
+    
     var name : String?
+    
+    var selectedAnimal: Animal?
+    
+    var displayAnimals: [String] = Animal.list.sorted()
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         title = name
-        // Do any additional setup after loading the view.
+        
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let displayAnimals : Int = displayAnimals.count
+          return displayAnimals
+      }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableCell", for: indexPath)
+
+        cell.textLabel?.text = displayAnimals[indexPath.row]
+        
+        return cell
+    }
+}
+    extension ATViewController: UISearchBarDelegate {
+        func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+
+        }
+
+        func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+            let findAnimal: String = searchText.trimmingCharacters(in: .whitespacesAndNewlines)
+            let isSearchTextNotEmpty = !findAnimal.isEmpty
+            let animals: [String] = Animal.list.sorted()
+            if isSearchTextNotEmpty {
+                let searchedAnimals: [String] = animals.filter {
+                    let findAnimal : String = findAnimal.lowercased()
+                    let animal = $0.lowercased()
+                    return animal.contains(findAnimal)
+                }
+                displayAnimals = searchedAnimals
+                
+            } else {
+                displayAnimals = animals
+            }
+            tableView.reloadData()
+        }
+        
     }
     
 
@@ -27,4 +71,3 @@ class ATViewController: UIViewController {
     }
     */
 
-}
