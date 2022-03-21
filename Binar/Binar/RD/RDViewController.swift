d360d6eecb5dd4d30d41bad10c12d7e528f8b879
raden-dimas012 <dimascode012@gmail.com>
2022-03-21 17:48:19 +0700

    edited cell animal

--- Binar/Binar/RD/RDViewController.swift
+++ Binar/Binar/RD/RDViewController.swift
@@ -9,23 +9,18 @@ import UIKit
 
 final class RDViewController: UIViewController {
     var name: String?
-    var displayedStudents: [String] = Animal.list.sorted()
+    private var displayedAnimals: [String] = Animal.list.sorted()
     
-    let tableview: UITableView = {
+ 
+    
+    private let tableView: UITableView = {
           let tv = UITableView()
           tv.backgroundColor = UIColor.white
+          tv.separatorColor = UIColor.white
           tv.translatesAutoresizingMaskIntoConstraints = false
           return tv
       }()
-    
-    let dayLabel: UILabel = {
-        let label = UILabel()
-        label.text = "Day 1"
-        label.textColor = UIColor.white
-        label.font = UIFont.boldSystemFont(ofSize: 16)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
+
     
     private let searchVC = UISearchController(searchResultsController: nil)
     
@@ -33,42 +28,62 @@ final class RDViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = name
-        self.view.backgroundColor = .white
         setupTableView()
         createSearchBar()
     }
     
-    func setupTableView() {
-        tableview.delegate = self
-        tableview.dataSource = self
-        tableview.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+    private func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(AnimalListCell.self, forCellReuseIdentifier: "cellId")
         
-        view.addSubview(tableview)
+        view.addSubview(tableView)
         
         NSLayoutConstraint.activate([
-            tableview.topAnchor.constraint(equalTo: self.view.topAnchor),
-            tableview.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
-            tableview.rightAnchor.constraint(equalTo: self.view.rightAnchor),
-            tableview.leftAnchor.constraint(equalTo: self.view.leftAnchor)
+            tableView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            tableView.rightAnchor.constraint(equalTo: self.view.rightAnchor),
+            tableView.leftAnchor.constraint(equalTo: self.view.leftAnchor)
         ])
+
     }
     
-    func createSearchBar() {
+    private func createSearchBar() {
         navigationItem.searchController = searchVC
         searchVC.searchBar.delegate = self
     }
     
-    
+    private func reloadDataTableView() {
+        let animals: [String] = Animal.list.sorted()
+        displayedAnimals = animals
+        tableView.reloadData()
+    }
     
 }
 
 extension RDViewController: UISearchBarDelegate {
-    
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        guard let text = searchBar.text,!text.isEmpty else {
-            return
+        
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        tableView.endEditing(true)
+        reloadDataTableView()
+    }
+
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        let _searchText: String = searchText.trimmingCharacters(in: .whitespacesAndNewlines)
+        let isSearchTextNotEmpty = !_searchText.isEmpty
+        let animals: [String] = Animal.list.sorted()
+        if isSearchTextNotEmpty {
+            let searchedAnimals: [String] = animals.filter {
+                let _searchText: String = searchText.lowercased()
+                let _name: String = $0.lowercased()
+                return _name.contains(_searchText)
+            }
+            displayedAnimals = searchedAnimals
         }
-        print(text)
+        else{
+           displayedAnimals = animals
+        }
+        tableView.reloadData()
     }
 }
 
@@ -79,34 +94,76 @@ extension RDViewController: UITableViewDelegate,UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return displayedStudents.count
+        return displayedAnimals.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableview.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath) as! AnimalListCell
         cell.backgroundColor = UIColor.white
-        cell.textLabel?.text = displayedStudents[indexPath.row]
+        cell.animalLabel.text = displayedAnimals[indexPath.row]
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let alertController = UIAlertController(title: "Touch", message: "You are touching a \(displayedStudents[indexPath.row])!", preferredStyle: .alert)
+        let alertController = UIAlertController(title: "Touch", message: "You touching a \(displayedAnimals[indexPath.row])!", preferredStyle: .alert)
         
-        let cancel = UIAlertAction(title: "Back", style: .default)
+        let cancel = UIAlertAction(title: "Back", style: .destructive)
         alertController.addAction(cancel)
         
         present(alertController, animated: true)
     }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
 }
 
-class ThirtyDayCell: UITableViewCell {
+class AnimalListCell: UITableViewCell {
+    
+    private let cellView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.blue
+        view.layer.cornerRadius = 10
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    var animalLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = UIColor.white
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setupView()
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupView() {
+        addSubview(cellView)
+        cellView.addSubview(animalLabel)
+        self.selectionStyle = .none
+        
+        NSLayoutConstraint.activate([
+            cellView.topAnchor.constraint(equalTo: self.topAnchor, constant: 20),
+            cellView.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -10),
+            cellView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 10),
+            cellView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+        
+        animalLabel.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        animalLabel.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        animalLabel.centerYAnchor.constraint(equalTo: cellView.centerYAnchor).isActive = true
+        animalLabel.leftAnchor.constraint(equalTo: cellView.leftAnchor, constant: 20).isActive = true
+        
     }
     
 }
