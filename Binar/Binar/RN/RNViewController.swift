fcdb72d8268707119aecbb3a3ebcf2878859f6ae
Randy Setiawan Hoesin <randysetiawanh@gmail.com>
2022-03-23 16:35:40 +0700

    Add Table View Controller and refactor controller to show list animals for table cell

--- Binar/Binar/RN/RNViewController.swift
+++ Binar/Binar/RN/RNViewController.swift
@@ -1,10 +1,26 @@
 import UIKit
 
-class RNViewController: UIViewController {
-
+final class RNViewController: UITableViewController {
+    
     var name : String?
+    var listAnimals: [String] = Animal.list.sorted()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = name
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let displayAnimals : Int = listAnimals.count
+          return displayAnimals
+      }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AnimalTableCell", for: indexPath)
+
+        cell.textLabel?.text = listAnimals[indexPath.row]
+        
+        return cell
+    }
+
 }
