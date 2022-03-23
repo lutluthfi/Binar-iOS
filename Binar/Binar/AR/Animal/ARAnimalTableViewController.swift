0666231c5b259e54241342a50916db8953c7a724
Arif Luthfiansyah <lutluthfiansyah@gmail.com>
2022-03-23 22:18:09 +0700

    Add example custom table view cell

--- Binar/Binar/AR/Animal/ARAnimalTableViewController.swift
+++ Binar/Binar/AR/Animal/ARAnimalTableViewController.swift
@@ -12,6 +12,7 @@ final class ARAnimalTableViewController: UITableViewController, StoryboardInstan
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.registerCell(ARAnimalTableCell.self)
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -20,16 +21,19 @@ final class ARAnimalTableViewController: UITableViewController, StoryboardInstan
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "AnimalTableCell", for: indexPath)
+        let reusableCell: UITableViewCell = tableView.dequeueReusableCell(
+            withIdentifier: "ARAnimalTableCell",
+            for: indexPath
+        )
+        
+        guard let cell = reusableCell as? ARAnimalTableCell else {
+            return reusableCell
+        }
         
         let row: Int = indexPath.row
         let animal: Animal = displayedAnimals[row]
         
-        cell.accessoryType = .disclosureIndicator
-        cell.textLabel?.text = animal.name
-        cell.detailTextLabel?.text = animal.description
-        cell.imageView?.loadImage(resource: animal.photoUrlString)
-        cell.imageView?.backgroundColor = .gray
+        cell.fill(with: animal)
         
         return cell
     }
