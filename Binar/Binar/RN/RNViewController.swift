924c741d54080d2b04e90fef20c696b58d419a81
Randy Setiawan Hoesin <randysetiawanh@gmail.com>
2022-03-23 16:45:41 +0700

    Add Search Bar and refactor enable function for Search Bar

--- Binar/Binar/RN/RNViewController.swift
+++ Binar/Binar/RN/RNViewController.swift
@@ -2,6 +2,8 @@ import UIKit
 
 final class RNViewController: UITableViewController {
     
+    @IBOutlet weak var animalSearchBar: UISearchBar!
+    
     var name : String?
     var listAnimals: [String] = Animal.list.sorted()
     
@@ -23,4 +25,29 @@ final class RNViewController: UITableViewController {
         return cell
     }
 
+}
+
+extension RNViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+
+    }
+
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        let findAnimal: String = searchText.trimmingCharacters(in: .whitespacesAndNewlines)
+        let isSearchTextNotEmpty = !findAnimal.isEmpty
+        let animals: [String] = Animal.list.sorted()
+        if isSearchTextNotEmpty {
+            let searchedAnimals: [String] = animals.filter {
+                let findAnimal : String = findAnimal.lowercased()
+                let animal = $0.lowercased()
+                return animal.contains(findAnimal)
+            }
+            listAnimals = searchedAnimals
+            
+        } else {
+            listAnimals = animals
+        }
+        tableView.reloadData()
+    }
+    
 }
