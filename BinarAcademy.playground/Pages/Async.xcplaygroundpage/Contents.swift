0ce5ad42d56057e2f1164009db93b698adbf6395
prana.wijaya <prana.wijaya@mncgroup.com>
2022-03-23 18:00:17 +0700

    Async await fetch data

--- BinarAcademy.playground/Pages/Async.xcplaygroundpage/Contents.swift
+++ BinarAcademy.playground/Pages/Async.xcplaygroundpage/Contents.swift
@@ -53,3 +53,59 @@ func syncFunc() {
 
 
 print(syncFunc())
+
+struct MyJsonModel: Codable {
+    var userId: Int = 0
+    var id: Int = 0
+    var title: String = "Agus"
+    var body: String = "lorem Ipsum"
+    
+}
+
+
+func goGrabSomething(completion: @escaping ([MyJsonModel]?, Error?) -> Void) {
+    let ourl = URL(string: "https://jsonplaceholder.typicode.com/posts")
+    if let url = ourl {
+        let req = URLRequest(url: url)
+        URLSession.shared.dataTask(with: req) { data, _, err in
+            guard let data = data, err == nil else {
+                return
+            }
+            do {
+                let model = try JSONDecoder().decode([MyJsonModel].self, from: data)
+                DispatchQueue.main.async {
+                    completion(model, nil)
+
+                }
+            } catch {
+                completion(nil, error)
+            }
+        }.resume()
+    }
+}
+
+goGrabSomething { result, Error in
+    guard let data = result else {
+        return
+    }
+    
+    print(data[0].title)
+}
+
+
+func goGrabSomethingAsyncAwait() async throws -> MyJsonModel? {
+    var model: MyJsonModel? = nil
+    let ourl = URL(string: "https://jsonplaceholder.typicode.com/posts")
+    if let url = ourl {
+        let req = URLRequest(url: url)
+        let (data, _) = try await URLSession.shared.data(for: req)
+        model = try JSONDecoder().decode(MyJsonModel.self, from: data)
+    }
+    return model
+}
+
+
+async {
+    var myModel = try await goGrabSomethingAsyncAwait()
+    print("Name: \(myModel.)")
+}
