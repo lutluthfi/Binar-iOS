6972e4372ad98578f3280021a285bcf7604f5a60
Maulana Frasha <ul.frasha@gmail.com>
2022-03-24 22:15:42 +0700

    add swipeaction and alert update. *still cant append

--- Binar/Binar/MF/Animal/MFFasilChallengeViewController.swift
+++ Binar/Binar/MF/Animal/MFFasilChallengeViewController.swift
@@ -11,7 +11,7 @@ final class MFFasilChallengeViewController: UIViewController, UITableViewDelegat
     
     var animalList: [Animal]  = Animal.listV2()
     @IBOutlet weak var tableView: UITableView!
-    
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -30,10 +30,39 @@ final class MFFasilChallengeViewController: UIViewController, UITableViewDelegat
         
         let animal: Animal = animalList[indexPath.row]
         reuseCell.cellData(with: animal)
-        
         return reuseCell
     }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let swipeDelete = UIContextualAction(style: .destructive, title: "Delete") { action, view, completion in
+            self.animalList.remove(at: indexPath.row)
+            tableView.reloadData()
+        }
+        
+        let swipeUpdate = UIContextualAction(style: .normal, title: "Update") { action, view, completion in
+            self.updateJSON()
+            tableView.reloadData()
+        }
+        
+        let config = UISwipeActionsConfiguration(actions: [swipeDelete, swipeUpdate])
+        return config
+    }
 
+    func updateJSON(){
+        
+        let alert = UIAlertController(title: "Change Name", message: "\n Input a new name.", preferredStyle: .alert)
+        alert.addTextField(configurationHandler: { textField in
+            textField.textAlignment = .center
+        })
+        
+        alert.addAction(UIAlertAction(title: "Update", style: .default, handler: { action in
+            let inputNama = alert.textFields![0]
+            self.animalList.append(Animal(name: inputNama.text!, photoUrlString: "", description: "Deskripsi", typeOfFood: .carnivora, strength: 1))
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        present(alert, animated: true)
+    }
 }
-
-
