36616f9ccaf00b4a926e3e1c35b6bbfd5a7658f3
danibukhory <dhanybukhory04@gmail.com>
2022-04-06 17:20:46 +0700

    Add challange 4 dan challange 2 Animal table view

--- Binar/Binar/PresentationLayer/DB/DBViewController.swift
+++ Binar/Binar/PresentationLayer/DB/DBViewController.swift
@@ -7,23 +7,19 @@
 
 import UIKit
 
-class DBViewController: UIViewController {
+final class DBViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
 
-        // Do any additional setup after loading the view.
-    }
+// Do any additional setup after loading the view.
+   }
+
+    @IBOutlet weak var challange4: UIButton!
+
+    @IBOutlet weak var challange2: UIButton!
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+
