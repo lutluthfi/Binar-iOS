9b7cdaaf016ade4cda7e3e4a189a3fbc654b042d
danibukhory <dhanybukhory04@gmail.com>
2022-04-22 00:52:32 +0700

    add challange repair

--- Binar/Binar/PresentationLayer/DB/Challange4/RoundButton.swift
+++ Binar/Binar/PresentationLayer/DB/Challange4/RoundButton.swift
@@ -7,16 +7,9 @@
 
 import UIKit
 
-@IBDesignable
-
 class RoundButton: UIButton {
 
     
     
-    
-    @IBInspectable var cornerRadius: CGFloat = 0 {
-        didSet{
-            self.layer.cornerRadius = cornerRadius
-        }
-    }
+
 }
