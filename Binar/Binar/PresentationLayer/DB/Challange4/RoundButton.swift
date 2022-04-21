501cbc1cd3199956174afd7a1dea4d7b2673b369
danibukhory <dhanybukhory04@gmail.com>
2022-04-22 00:52:32 +0700

    Revert "add challange repair"
    This reverts commit 9b7cdaaf016ade4cda7e3e4a189a3fbc654b042d.

--- Binar/Binar/PresentationLayer/DB/Challange4/RoundButton.swift
+++ Binar/Binar/PresentationLayer/DB/Challange4/RoundButton.swift
@@ -7,9 +7,16 @@
 
 import UIKit
 
+@IBDesignable
+
 class RoundButton: UIButton {
 
     
     
-
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet{
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
 }
