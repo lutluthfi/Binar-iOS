8215d78abc4b4e3216e8566700bc449f7ae59650
Arif Luthfiansyah <lutluthfiansyah@gmail.com>
2022-05-11 08:45:34 +0700

    Add clearCells for LiteTableView

--- Binar/Binar/PresentationLayer/Instagram/Profile/IGProfileViewController.swift
+++ Binar/Binar/PresentationLayer/Instagram/Profile/IGProfileViewController.swift
@@ -7,6 +7,34 @@
 
 import UIKit
 
-final class IGProfileViewController: UIViewController {
+final class IGProfileViewController: LiteTableViewController {
+    @UserDefaultsArray<String>(key: "bookmark-feed-id") private var storageBookmarkFeedIds
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        renderTableView()
+    }
+    
+    private func renderTableView() {
+        loadTableView {
+            loadCell { (cell: TableCell<UIButton>, _) in
+                cell.padding = UIEdgeInsets(all: 4)
+                cell.content.setTitle("Reload", for: .normal)
+                cell.content.backgroundColor = .systemBlue
+                cell.content.addTarget(self, action: #selector(self.onReloadTap), for: .touchUpInside)
+            }
+            forEachElement(in: storageBookmarkFeedIds) { row, element in
+                loadCell { (cell: TableCell<UILabel>, _) in
+                    cell.padding = UIEdgeInsets(all: 4)
+                    cell.content.text = element
+                }
+                rectangle(height: 1, color: .gray)
+            }
+        }
+    }
+    
+    @objc private func onReloadTap() {
+        clearCells()
+        renderTableView()
+    }
 }
