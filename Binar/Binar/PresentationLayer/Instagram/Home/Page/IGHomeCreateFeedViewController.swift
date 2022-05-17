9982d76bddc06d41d997124ee1a5eab89369c284
Arif Luthfiansyah <lutluthfiansyah@gmail.com>
2022-05-17 21:47:06 +0700

    Update to implement ImagePicker

--- Binar/Binar/PresentationLayer/Instagram/Home/Page/IGHomeCreateFeedViewController.swift
+++ Binar/Binar/PresentationLayer/Instagram/Home/Page/IGHomeCreateFeedViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Arif Luthfiansyah on 17/05/22.
 //
 
+import MobileCoreServices
 import UIKit
 
 final class IGHomeCreateFeedViewController: UIViewController {
@@ -13,6 +14,9 @@ final class IGHomeCreateFeedViewController: UIViewController {
         view.contentMode = .scaleAspectFit
         view.clipsToBounds = true
         view.backgroundColor = .systemGray
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(onImageViewTap))
+        view.isUserInteractionEnabled = true
+        view.addGestureRecognizer(tapGesture)
         return view
     }()
     lazy var captionTextField: UITextField = {
@@ -20,6 +24,8 @@ final class IGHomeCreateFeedViewController: UIViewController {
         view.placeholder = "Write your caption here"
         return view
     }()
+    
+    private var heightImageViewConstraint: NSLayoutConstraint?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,11 +41,13 @@ final class IGHomeCreateFeedViewController: UIViewController {
     }
     
     private func setupConstraint() {
+        let imageViewHeight: CGFloat = view.frame.height / 2
+        heightImageViewConstraint = imageView.heightAnchor.constraint(equalToConstant: imageViewHeight)
         imageView.makeConstraint(builder: {
             $0.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor)
             $0.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
             $0.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
-            $0.bottomAnchor.constraint(equalTo: view.centerYAnchor)
+            heightImageViewConstraint!
         })
         captionTextField.makeConstraint(builder: {
             $0.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20)
@@ -59,5 +67,45 @@ final class IGHomeCreateFeedViewController: UIViewController {
     
     @objc private func onNextBarButtonTap() {
         print("Next")
+    }
+    
+    @objc private func onImageViewTap() {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.allowsEditing = true
+        pickerController.mediaTypes = [String(kUTTypeImage), String(kUTTypeMovie)]
+        pickerController.sourceType = .photoLibrary
+        present(pickerController, animated: true)
+    }
+}
+
+extension IGHomeCreateFeedViewController: UINavigationControllerDelegate {
+}
+
+extension IGHomeCreateFeedViewController: UIImagePickerControllerDelegate {
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        guard let image = info[.editedImage] as? UIImage else {
+            return
+        }
+        picker.dismiss(animated: true) { [weak self] in
+            self?.didImagePickerControllerDismiss(image: image)
+        }
+    }
+    
+    private func didImagePickerControllerDismiss(image: UIImage) {
+        let imageRatio: CGFloat = image.size.width / image.size.height
+        var imageHeight: CGFloat = view.frame.width / imageRatio
+        imageHeight = ceil(imageHeight)
+        
+        heightImageViewConstraint?.constant = imageHeight
+        imageView.image = image
+        
+        UIView.animate(withDuration: 0.2) {
+            self.view.layoutIfNeeded()
+        }
     }
 }
