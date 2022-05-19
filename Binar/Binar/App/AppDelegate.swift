33abfe07d52025d310b16ebe5717bdc192321ce7
Arif Luthfiansyah <lutluthfiansyah@gmail.com>
2022-05-19 22:39:34 +0700

    Update to implement Remote Command Center for Music

--- Binar/Binar/App/AppDelegate.swift
+++ Binar/Binar/App/AppDelegate.swift
@@ -5,6 +5,7 @@
 //  Created by Arif Luthfiansyah on 11/03/22.
 //
 
+import AVFoundation
 import Firebase
 import FirebaseCore
 import IQKeyboardManagerSwift
@@ -19,9 +20,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
         FirebaseApp.configure()
-        IQKeyboardManager.shared.enable = true
-        
         RemoteConfigHelper.standard.prepareValue()
+        IQKeyboardManager.shared.enable = true
         
         return true
     }
